78883d1f22e86ba72a9126a9efdc44fc0ac461fe
Rowan <malice.peanutbutter@gmail.com>
2025-03-08 21:56:45 +0200

    Bsearch

--- binarySearch/binarySearch.ts
+++ binarySearch/binarySearch.ts
@@ -1,5 +1,24 @@
 export function binarySearch(arrayOfThings: number[], target: number): number {
-	console.log(arrayOfThings);
-	console.log(target);
+	const middleIndex = Math.floor(arrayOfThings.length / 2);
+	console.log(arrayOfThings[middleIndex]);
+
+	if (target === arrayOfThings[middleIndex]) return middleIndex;
+	if (target > arrayOfThings[middleIndex]) {
+		const right = [
+			...arrayOfThings.filter((x) => x > arrayOfThings[middleIndex]),
+		];
+
+		console.log({ right });
+		binarySearch(right, target);
+	}
+
+	if (target < arrayOfThings[middleIndex]) {
+		const left = [
+			...arrayOfThings.filter((x) => x < arrayOfThings[middleIndex]),
+		];
+		console.log({ left });
+		binarySearch(left, target);
+	}
+
 	return -1;
 }
--- binarySearch/binarySearch_test.ts
+++ binarySearch/binarySearch_test.ts
@@ -5,4 +5,5 @@ Deno.test(function binarySearchTest() {
 	assertEquals(binarySearch([1, 2, 3, 4, 5, 6], 5), 4);
 	assertEquals(binarySearch([1, 2, 3, 4, 5, 6], 6), 5);
 	assertEquals(binarySearch([1, 2, 3, 4, 5, 6], 7), -1);
+	assertEquals(binarySearch([-1, 0, 3, 5, 9, 12], 7), -1);
 });
